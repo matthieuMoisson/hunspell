e3c56e6327371d9731bb79a9bf7fad2a56da4ba7
matthieu-moisson-franckhauser <matthieu.moisson-franckhauser@bedrockstreaming.com>
2022-04-11 08:27:23 +0200

    feat first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+run main with args : 
+src/main/ressources/fr-classique.dic src/main/ressources/fr-classique.aff src/main/ressources/tata.csv
---
+++ src/main/scala/com/matthieu/hunspell/app.scala
@@ -0,0 +1,39 @@
+package com.matthieu.hunspell
+
+import com.atlascopco.hunspell.Hunspell
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+
+import scala.collection.convert.ImplicitConversions.`list asScalaBuffer`
+
+object app {
+
+  def getWord(sentence: String) : String = {
+    sentence.split(" ")(1).split(":")(1)
+  }
+
+  def main (args: Array[String]): Unit = {
+    if(args.size != 3)
+      throw new Exception("Need 3 args, dic, aff, and file")
+    val pathDic: String = args(0)
+    val pathAff: String = args(1)
+    val pathFile: String = args(2)
+
+    lazy val spark: SparkSession = SparkSession
+      .builder
+      .appName(s"Hunspell")
+      .config("spark.master", "local")
+      .getOrCreate
+
+    val words: DataFrame = spark.read.text(pathFile)
+    import spark.implicits._
+    words.show()
+    val lemmas: Dataset[String] = words.map(word => {
+      val speller = new Hunspell(pathDic, pathAff)
+      val lemmas = speller.analyze(word.getString(0)).toList.map(lemma => getWord(lemma)).distinct
+      lemmas.mkString("+")
+    })
+
+    lemmas.show(false)
+
+  }
+}
